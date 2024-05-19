bd67a8a521398bbb8399aeaf7d3b33e05dc0082d
yamoo9 <yamoo9@naver.com>
2024-05-19 21:21:56 +0900

    [구성] ECMAScript

---
+++ ecmascript/05-destructuring-assignment.js
@@ -0,0 +1,115 @@
+// --------------------------------------------------------------------------
+// destructuring assignment
+// - 구조 분해 할당 구문을 사용해 배열의 개별 항목 분해 및 할당
+// - 구조 분해 할당 구문을 사용해 객체의 개별 항목 분해 및 할당
+// - 콜백 함수의 매개변수를 구조 분해 할당하여 활용
+// --------------------------------------------------------------------------
+
+const courses = [
+  {
+    id: 1,
+    title: 'Vue 펀더멘탈',
+    url: 'https://fundamentals.dev/vue',
+  },
+  {
+    id: 2,
+    title: 'Vue Router 펀더멘탈',
+    url: 'https://fundamentals.dev/vue-rouer',
+  },
+  {
+    id: 3,
+    title: 'Pinia 펀더멘탈',
+    url: 'https://fundamentals.dev/pinia',
+  },
+];
+
+function spreadArray() {
+  {
+    const vueCourse = courses[0];
+    const restCourses = courses.slice(1);
+
+    console.log(vueCourse);
+    console.log(restCourses);
+  }
+
+  // 🔶 구조 분해 할당 구문을 사용해 courses 배열에서 항목을 분해 및 할당합니다.
+  // 참고: https://mzl.la/3Jfrwpm
+
+  // console.log({ course: vueCourse });
+  // console.log({ rest: restCourses });
+}
+
+function spreadObject() {
+  const [vueCourse] = courses;
+
+  {
+    let vueCourseId = vueCourse.id;
+    let vueCourseTitle = vueCourse.title;
+    let vueCourseUrl = vueCourse.url;
+
+    console.log(vueCourseId);
+    console.log(vueCourseTitle);
+    console.log(vueCourseUrl);
+  }
+
+  // 🔶 구조 분해 할당 구문을 사용해 vueCourse 객체에서 항목을 분해 및 할당합니다.
+  // 참고: https://mzl.la/3Jfrwpm
+
+  // console.log({ courseId });
+  // console.log({ courseTitle });
+  // console.log({ courseUrl });
+}
+
+function spreadRender() {
+  const koreanFoods = {
+    caption: '한식 메뉴',
+    rows: [
+      { headline: '뚝배기 불고기', content: 8000 },
+      { headline: '스팸치즈볶음밥', content: 7500 },
+      { headline: '불고기낙지덮밥', content: 9000 },
+    ],
+  };
+
+  const rendredResult = renderTable(koreanFoods);
+  console.log(rendredResult);
+
+  function renderTable(data) {
+    return removeSpaceHTMLString(/* html */ `
+      <table class="table">
+        <caption class="sr-only">${data.caption}</caption>
+        ${data.rows.reduce(function (htmlString, item) {
+          // 🔶 구조 분해 할당 구문을 사용해 item 객체에서 항목을 분해 및 할당합니다.
+          // 참고: https://mzl.la/3Jfrwpm
+
+          return (
+            htmlString +
+            /* html */ `
+              <tr>
+                <th>${item.headline}</th>
+                <td>${numberWithComma(item.content)}원</td>
+              </tr>
+            `
+          );
+        }, '')}
+      </table>
+    `);
+  }
+}
+
+function numberWithComma(numberValue) {
+  return numberValue.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
+
+function removeSpaceHTMLString(htmlString) {
+  return htmlString.replace(/\s+<|\n|>\s+/g, function ($1) {
+    return $1.indexOf('<') > -1 ? '<' : $1.indexOf('>') > -1 ? '>' : '';
+  });
+}
+
+function run() {
+  spreadArray();
+  spreadObject();
+  spreadRender();
+}
+
+run();
---
+++ ecmascript/09-promise-async-await.js
@@ -0,0 +1,84 @@
+// --------------------------------------------------------------------------
+// Promise, Async await
+
+function practice() {
+  console.log('start!');
+
+  // callback hell
+  // 참고: https://bit.ly/3r5iUfe
+  delay(() => {
+    console.log('1s');
+    delay(() => {
+      console.log('2s');
+      delay(() => {
+        console.log('3s');
+        delay(() => {
+          console.log('4s');
+          console.log('end!');
+        });
+      });
+    });
+  });
+}
+
+function delay(callback, time = 1000) {
+  setTimeout(callback, time);
+}
+
+// practice();
+
+// --------------------------------------------------------------------------
+
+// Promise
+// 참고: https://mzl.la/3d1He5h
+// 🔶 delayPromise 함수를 작성합니다.
+const delayPromise = () => {};
+
+function practice2() {
+  console.log('start!');
+  // 🔶 delayPromise 함수를 사용해 1초마다 로그를 남기도록 작성합니다.
+}
+
+// practice2();
+
+// Async Await
+// 참고: https://mzl.la/49EvJxn
+
+function practice3() {
+  // 🔶 비동기 함수를 사용해 delayPromise 함수가 1초마다 로그를 남기도록 작성합니다.
+}
+
+// practice3();
+
+// --------------------------------------------------------------------------
+// Promise.all, Promise.race
+
+const MAX_TIMEOUT = 1000;
+
+const promise1 = () =>
+  new Promise((resolve) => {
+    const timeout = Math.random() * MAX_TIMEOUT;
+    setTimeout(resolve, timeout, {
+      value: 'X',
+      timeout: `${timeout.toFixed(2)}s`,
+    });
+  });
+
+const promise2 = () =>
+  new Promise((resolve) => {
+    const timeout = Math.random() * MAX_TIMEOUT;
+    setTimeout(resolve, timeout, {
+      value: 'Y',
+      timeout: `${timeout.toFixed(2)}s`,
+    });
+  });
+
+const promise3 = () => Promise.reject('❌ 오류 발생!');
+
+// Promise.all
+// 참고: https://mzl.la/4dM0fsr
+// 🔶 Promise.all 메서드를 사용해 모든 Promise가 실행된 이후 콜백되도록 실습을 진행합니다.
+
+// Promise.race
+// 참고: https://mzl.la/44MaIQy
+// 🔶 Promise.race 메서드를 사용해 응답이 가장 빠른 Promise 값만 출력되도록 실습을 진행합니다.
