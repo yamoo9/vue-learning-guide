abafb7dd6aba1eef26e86bc2b704afe8a8d8e759
yamoo9 <yamoo9@naver.com>
2024-05-30 22:24:32 +0900

    [구성] 비동기 컴포넌트

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import Navigation from '@/components/AppNavigation.vue';
+import ComponentFundamentals from '@/views/ComponentFundamentals.vue';
+import RegisterForm from '@/views/RegisterForm.vue';
+import PropsDrilling from '@/views/PropsDrilling/PropsDrilling.vue';
+
+const navigationList = ref<INavigationItem[]>([
+  {
+    id: 1,
+    href: '/',
+    viewMode: 'home',
+  },
+  {
+    id: 2,
+    href: '/form',
+    viewMode: 'form',
+  },
+  {
+    id: 3,
+    href: '/props-drilling',
+    viewMode: 'props drilling',
+  },
+]);
+
+const renderView = ref<RenderView>('props drilling');
+
+const changeRenderView = (viewMode: RenderView) => {
+  renderView.value = viewMode;
+};
+
+const handleChangeRenderView = (viewMode: RenderView, payload?: FormPayload) => {
+  changeRenderView(viewMode === 'submitted' ? 'home' : viewMode);
+  if (payload) alert(JSON.stringify(payload, null, 2));
+};
+
+const render = computed(() => {
+  switch (renderView.value) {
+    default:
+    case 'home':
+      return {
+        component: ComponentFundamentals,
+        eventHandlers: {},
+      };
+    case 'form':
+      return {
+        component: RegisterForm,
+        eventHandlers: { changeRenderView: handleChangeRenderView },
+      };
+    case 'props drilling':
+      return {
+        component: PropsDrilling,
+        eventHandlers: {},
+      };
+  }
+});
+</script>
+
+<template>
+  <Navigation
+    :list="navigationList"
+    :renderView="renderView"
+    @changeRenderView="handleChangeRenderView"
+  />
+  <KeepAlive :max="10">
+    <component :is="render.component" v-on="render.eventHandlers"></component>
+  </KeepAlive>
+</template>
---
+++ src/views/PropsDrilling/GrandChild.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { inject } from 'vue';
+import WrapperBox from '@/components/WrapperBox.vue';
+import type { IIsShowChildren } from './PropsDrilling.vue';
+import { isShowChildrenSymbol } from './symbolKeys';
+
+const { isShow } = inject(isShowChildrenSymbol) as IIsShowChildren;
+</script>
+
+<template>
+  <WrapperBox title="Grand Child" color="#705af9" :style="`opacity: ${isShow ? 1 : 0}`" />
+</template>
