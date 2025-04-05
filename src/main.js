371469f24497eae784329264e1b7edbba9c02be3
yamoo9 <yamoo9@naver.com>
2025-04-05 15:34:00 +0900

    [정리] 학습 자료

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
-import './styles/globals.css';
+import './styles/globals.css'
 import {
   createApp,
-  ref,
   reactive,
   computed,
-} from 'https://esm.sh/vue/dist/vue.esm-browser.js';
+  ref,
+} from 'https://esm.sh/vue/dist/vue.esm-browser.js'
 
-const vueApp = createApp({
+const app = createApp({
   setup() {
-    const renderView = ref('form');
-    const isFormRender = computed(() => renderView.value.includes('form'));
+    const renderView = ref('form')
+    const isFormRender = computed(() => renderView.value.includes('form'))
+    const toggleButtonLabel = computed(() => isFormRender.value ? '리스트 보기' : '폼 보기')
 
-    let commentId = 1;
+    let commentId = 1
     const comments = reactive([
       {
         id: commentId++,
@@ -21,28 +22,27 @@ const vueApp = createApp({
       {
         id: commentId++,
         title: 'Vue.js는 빠르고 가볍습니다.',
-        content:
-          '자체 최적화 기술로 성능을 최대한 끌어올려주며, 번들된 파일 크기도 작습니다. 자체 최적화 기술로 성능을 최대한 끌어올려주며, 번들된 파일 크기도 작습니다.',
+        content: '자체 최적화 기술로 성능을 최대한 끌어올려주며, 번들된 파일 크기도 작습니다. 자체 최적화 기술로 성능을 최대한 끌어올려주며, 번들된 파일 크기도 작습니다.',
       },
-    ]);
+    ])
 
     const handleChangeView = () => {
-      renderView.value = isFormRender.value ? 'comments' : 'form';
-    };
+      renderView.value = isFormRender.value ? 'comments' : 'form'
+    }
 
-    return { isFormRender, comments, handleChangeView };
+    return { isFormRender, toggleButtonLabel, comments, handleChangeView }
   },
   template: /* html */ `
     <button
       type="button"
       class="changeViewButton"
       @click="handleChangeView"
+      :aria-label="toggleButtonLabel"
+      :title="toggleButtonLabel"
     >
-      <svg v-if="isFormRender" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"></path>
-      </svg>
-      <svg v-else fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z"></path>
+      <svg fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path v-show="isFormRender" stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z"></path>
+        <path v-show="!isFormRender" stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"></path>
       </svg>
     </button>
 
@@ -69,6 +69,6 @@ const vueApp = createApp({
       </article>
     </section>
   `,
-});
+}).mount('#app')
 
-globalThis.vm = vueApp.mount('#app');
+globalThis.vm = app
